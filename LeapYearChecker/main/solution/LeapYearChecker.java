093bd806c88c729aa44b617ecb601dfc92851a4b
dci-student <Terissa>
2023-10-16 15:42:35 +0200

    Leap Year Checker

---
+++ LeapYearChecker/main/solution/LeapYearChecker.java
@@ -0,0 +1,21 @@
+import java.time.Year;
+
+public class LeapYearChecker {
+    public static void main(String[]args){
+
+        //Define the years Define the years of birth for Suze, Karl, and Kaiser.
+        int suzeBirthYear = 2000;
+        int karlBirthYear = 1993;
+        int kaiserBirthYear = 1855;
+
+        //Check if the years are leap years Use the isLeap()
+        boolean isSuzeBirthYearLeap = Year.isLeap(suzeBirthYear);
+        boolean isKarlBirthYearLeap = Year.isLeap(karlBirthYear);
+        boolean isKaiserBirthYearLeap = Year.isLeap(kaiserBirthYear);
+
+        System.out.println(suzeBirthYear + " is a leap year: " + isSuzeBirthYearLeap);
+        System.out.println(karlBirthYear + " is a leap year: " + isKarlBirthYearLeap);
+        System.out.println(kaiserBirthYear + " is a leap year: " + isKaiserBirthYearLeap);
+
+    }
+}
