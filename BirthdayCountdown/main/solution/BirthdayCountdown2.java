618e2a2f5b579adc9e371fcfa8d9d5d20736863b
dci-student <Terissa>
2023-10-17 14:24:38 +0200

    Birthday Countdown

--- BirthdayCountdown/main/solution/BirthdayCountdown2.java
+++ BirthdayCountdown/main/solution/BirthdayCountdown2.java
@@ -1,7 +1,8 @@
 import java.time.LocalDate;
 import java.time.Period;
+import java.time.temporal.ChronoUnit;
 
-    public class BirthdayCountdown2 {
+public class BirthdayCountdown2 {
         public static void main(String[] args) {
             LocalDate today = LocalDate.now();
             LocalDate birthday = LocalDate.of(1993, 9, 7);
@@ -13,9 +14,15 @@ import java.time.Period;
             }
 
             // Calculate the period between today and the next birthday
-            Period period = Period.between(today, nextBirthday);
+            //Period period = Period.between(today, nextBirthday); //Period.between doesn't work here.
+            // It gives wrong result. Use ChronoUnit.DAYS
+            long daysLeft = ChronoUnit.DAYS.between(today, nextBirthday);
 
-            int daysLeft = period.getDays();
+            //Period period = Period.between(today, nextBirthday);
+            //int daysLeft = period.getDays();
+
+            //long daysLeft = period.getDays(); with    public static void main(String[] args) {
+            // with ChronoUnit.DAYS, you don't need to call 'period.getDays()'
             System.out.println("Days left until Terissa's next birthday: " + daysLeft);
         }
     }
